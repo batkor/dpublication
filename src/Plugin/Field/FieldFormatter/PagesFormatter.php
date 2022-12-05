b26d1a551bbafeb77aab6f6e2d16957304a089a2
batkor <info@batkor.ru>
2022-12-05 23:54:13 +0800

    #6: Add actions variable on template

--- src/Plugin/Field/FieldFormatter/PagesFormatter.php
+++ src/Plugin/Field/FieldFormatter/PagesFormatter.php
@@ -6,6 +6,10 @@ use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceFormatterBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'Pages' formatter.
@@ -18,7 +22,62 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class PagesFormatter extends EntityReferenceFormatterBase {
+class PagesFormatter extends EntityReferenceFormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current user proxy.
+   */
+  protected AccountProxyInterface $accountProxy;
+
+  /**
+   * Constructors.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\Core\Session\AccountProxyInterface $accountProxy
+   *   The current user proxy.
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    $label,
+    $view_mode,
+    array $third_party_settings,
+    AccountProxyInterface $accountProxy
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->accountProxy = $accountProxy;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('current_user')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -103,6 +162,14 @@ class PagesFormatter extends EntityReferenceFormatterBase {
           'entity' => $entity,
         ];
       }
+    }
+
+    /** @var \Drupal\dpublication\Entity\PublicationInterface $publication */
+    $publication = $items->getEntity();
+    if ($this->accountProxy->hasPermission("edit own {$publication->id()} publication")) {
+      $element['#actions'][] = Link::createFromRoute($this->t('Add page'), 'entity.publication_page.add_form', [
+        'publication' => $publication->id(),
+      ])->toRenderable();
     }
 
     return [$element];
